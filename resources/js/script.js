a02d2e75bdae599194730187710f13ec081171d6
papuaganteng <nurull.sarang@gmail.com>
2020-01-19 18:32:25 +0900

    nurulcode

--- resources/js/script.js
+++ resources/js/script.js
@@ -78,7 +78,7 @@ $(document).ready(function() {
 
   $(".js--wp-1").waypoint(
     function(direction) {
-      $(".js--wp-1").addClass("animated bounceInDown");
+      $(".js--wp-1").addClass("animated fadeIn");
     },
     {
       offset: "40%"
